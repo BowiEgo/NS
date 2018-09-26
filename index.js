416b881efa77fbcc82ceeaef4ff7f8bf32824261
bowiego <bobosndr@163.com>
2018-09-26 16:56:20 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './view/App';
+import {name as appName} from './view/app.json';
+
+AppRegistry.registerComponent(appName, () => App);
